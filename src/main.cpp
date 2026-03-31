927b8e210ee3ef7b3c68810ea9e07d88879585ae
GirishT-debug <Tandabanygir@ashland.k12.ma.us>
2026-03-31 19:20:48 +0000

    Test commit for Ashland High School Robotics Club Vex Competition

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,9 @@
+#include "vex.h"
 
+using namespace vex;
+
+int main() {
+    // Test commit for Ashland High School Robotics Club Vex Competition
+    Brain.Screen.print("Hello, Ashland Robotics!");
+    return 0;
+}
